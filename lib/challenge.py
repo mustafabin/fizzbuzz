b6ef715a2a81e3cdd0b296487b05b4bf260443fa
Mustafa <mbin828@gmail.com>
2022-04-20 15:30:18 -0400

    bonus done

--- lib/challenge.py
+++ lib/challenge.py
@@ -1,9 +1,11 @@
-for i in range(1,101):
-  if  i%3 == 0 and i%5 == 0:
-      print("fizzbuzz")
-  elif i%3==0:
-      print("fizz")
-  elif i%5==0:
-      print("buzz")
-  else:
-      print(i)
+topRange = input("enter top range: ")
+
+for i in range(1, int(topRange)):
+    if i % 3 == 0 and i % 5 == 0:
+        print("fizzbuzz")
+    elif i % 3 == 0:
+        print("fizz")
+    elif i % 5 == 0:
+        print("buzz")
+    else:
+        print(i)
